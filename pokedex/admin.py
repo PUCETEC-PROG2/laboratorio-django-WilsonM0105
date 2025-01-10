7cfef0356208186ce1f39ff81ee87358cd65926e
WilsonMites <wilsonmites1@gmail.com>
2025-01-10 11:02:15 -0500

    Incompleto laboratorio 9

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -3,11 +3,9 @@ from .models import Pokemon, Trainer
 
 # Register your models here.
 @admin.register(Pokemon)
-
 class PokemonAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Trainer)
-
 class TrainerAdmin(admin.ModelAdmin):
     pass
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Pokemon, Trainer
+from django.shortcuts import redirect, render
+from pokedex.forms import PokemonForm
 
 def index(request):
-    pokemons = Pokemon.objects.order_by('type')
+    pokemons = Pokemon.objects.all()
     trainers = Trainer.objects.all()
     template = loader.get_template('index.html')
     return HttpResponse(template.render({
@@ -20,10 +22,21 @@ def pokemon(request, pokemon_id):
     }
     return HttpResponse(template.render(context, request))
 
-def trainer_detail(request, trainer_id):
+def trainer_details(request, trainer_id):
     trainer = Trainer.objects.get(id=trainer_id)
     template = loader.get_template('display_trainer.html')
     context = {
         'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save
+            return redirect('pokemon:index')
+    else:
+        form = PokemonForm()
+    
+    return render(request, 'pokemon_form.html', {'form': form}) 
