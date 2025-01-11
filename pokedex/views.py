df7d53a4862af8761e8f97a2782770f8099cc3ec
WilsonMites <wilsonmites1@gmail.com>
2025-01-10 20:40:57 -0500

    Laboratorio 9 Completado

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon, Trainer
 from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+
+from .models import Pokemon, Trainer
 from pokedex.forms import PokemonForm
 
 def index(request):
@@ -15,7 +18,7 @@ def index(request):
         request))
 
 def pokemon(request, pokemon_id):
-    pokemon = Pokemon.objects.get(id=pokemon_id)
+    pokemon = Pokemon.objects.get(id = pokemon_id)
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
@@ -30,13 +33,35 @@ def trainer_details(request, trainer_id):
     }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def add_pokemon(request):
     if request.method == "POST":
         form = PokemonForm(request.POST, request.FILES)
         if form.is_valid():
             form.save
-            return redirect('pokemon:index')
+            return redirect('pokedex:index')
     else:
         form = PokemonForm()
+    return render(request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def edit_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def delete_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
+    pokemon.delete()
+    return redirect('pokedex:index')
+
+class CustomLoginView(LoginView):
+    template_name = "login_form.html"
     
-    return render(request, 'pokemon_form.html', {'form': form}) 
